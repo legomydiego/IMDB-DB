cb2d701d9e887a8b7b97c03983a05bfd309d014f
Diego Vargas <diegofvargas@gmail.com>
2017-11-20 16:24:13 -0500

    Mods

--- src/movies.js
+++ src/movies.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Movies extends Component {
     render() {
         return <ul>{this.props.list_of_movies.map((values, i) => {
-            return <li key={i}><img src={values.Poster} />{values.Title}</li>;
+            return <span key={i}>{values.Title} <img src={values.Poster} /></span>;
         })}</ul>;
     }
 }
